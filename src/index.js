a8dd589c90eb692de14f753484d947829266a9a2
no9 <mrcowsy@gmail.com>
2015-08-09 15:28:58 +0100

    initial share model and pouchdb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 var h = require('hyperscript')
+var Shares = require('./shares')
+var shares = new Shares()
 
-document.getElementById('page').appendChild(
+shares.all(function (err, data) {
+  for (var i = 0; i < data.length; i++) {
+    document.getElementById('page').appendChild(
+      h('div', 'SOME TEXT')
+    )
+  }
+})
+/*document.getElementById('page').appendChild(
   h('div.row#options',
     h('button#group', 'groups'),
     h('button#group', 'shares')
   )
-)
+)*/
--- src/shares.js
+++ src/shares.js
@@ -1,6 +1,29 @@
-var h = require('hyperscript')
+var PouchDB = require('pouchdb')
 
-exports = function() {
-	return h('button#shares', 'add')
+function Shares() {
+  this.db = new PouchDB('shares')
+  this.remoteCouch = false;	
 }
 
+Shares.prototype.all = function (cb) {
+  this.db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+    console.log(doc.rows)
+    cb(err, doc.rows)
+  })
+}
+
+Shares.prototype.add = function (name, available, cb) {
+  var share = {
+    _id: new Date().toISOString(),
+    name: name,
+    isAvailable: available
+  };
+  this.db.put(share, function callback(err, result) {
+    if (!err) {
+      cb(err, result)
+      console.log('Successfully posted a todo!');
+    }
+  });
+}
+
+module.exports = Shares
